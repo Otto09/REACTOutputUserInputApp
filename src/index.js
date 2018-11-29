a8f2fb652ac1d6c3f38f9063a4f736c85a3f08ae
Goga Octavian <ttgoga@yahoo.com>
2018-11-29 02:54:29 +0200

    Initial commit

--- src/index.js
+++ src/index.js
@@ -1,12 +1,66 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
-
-ReactDOM.render(<App />, document.getElementById('root'));
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+class AnimalsInZoo extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = { animals: [], name: '' };
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+  
+    render() {
+        return (
+            <div>
+                <h3>ZOO</h3>
+                <AnimalsList animals={this.state.animals} />
+                <form onSubmit={this.handleSubmit}>
+                    <label htmlFor="new-animals">
+                        What animals do you have in the zoo?
+                    </label><br/>
+                    <input
+                        id="new-animals"
+                        onChange={this.handleChange}
+                        value={this.state.name}
+                    /><br/>
+                    <button>
+                        Animal #{this.state.animals.length + 1}
+                    </button>
+                </form>
+            </div>
+        );
+    }
+  
+    handleChange(e) {
+        this.setState({ name: e.target.value });
+    }
+  
+    handleSubmit(e) {
+        e.preventDefault();
+        if (!this.state.name.length) {
+            return;
+        }
+        const newAnimal = {
+            name: this.state.name,
+            id: Date.now()
+        };
+        this.setState(state => ({
+            animals: state.animals.concat(newAnimal),
+            name: ''
+        }));
+    }
+}
+  
+class AnimalsList extends React.Component {
+    render() {
+        return (
+            <ol>
+                {this.props.animals.map(animal => (
+                    <li key={animal.id}>{animal.name}</li>
+                ))}
+            </ol>
+        );
+    }
+}
+  
+ReactDOM.render(<AnimalsInZoo />, document.getElementById('root'));
